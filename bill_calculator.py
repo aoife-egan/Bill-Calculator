6a07ef7a783fff7084de2a64e5a2b9bcea09a77f
S00167887 <S00167887@mail.itsligo.ie>
2019-10-08 23:32:50 +0100

    Some error handling

--- bill_calculator.py
+++ bill_calculator.py
@@ -1,6 +1,9 @@
 bill = input("How much is your original bill?")
-bill_value = int(bill)
-
+try:
+  bill_value = int(bill)
+except ValueError:
+  print("n\You must enter a valid number!")
+  
 def tax(bill_value):
   print("\nAdding 8% tax to a restaurant bill.")
   bill_value *= 1.08
@@ -10,7 +13,10 @@ def tax(bill_value):
   
 def tip(bill_value):
   tip_val = input("\nWhat % of a tip would you like to give?")
-  tip_value = int(tip_val)
+  try:
+    tip_value = int(tip_val)
+  except ValueError:
+    print("\nYou must enter a valid number!")
   print(f"\nAdding {tip_value:.0f}% tip to a restaurant bill.")   
   bill_value += ((bill_value * tip_value) / 100)
   print (f"\nWith {tip_value:.0f}% tip: {bill_value:.2f}")
