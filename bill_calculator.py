64b2d382b99deb8fe3ba4333878c6301cc6df76c
S00167887 <S00167887@mail.itsligo.ie>
2019-10-08 23:18:43 +0100

    adding tip value to the bill

--- bill_calculator.py
+++ bill_calculator.py
@@ -7,4 +7,14 @@ def tax(bill_value):
   print ("\nWith tax: %f" %(bill_value))
   return (bill_value)
 
+  
+def tip(bill_value):
+  tip_val = input("\nWhat % of a tip would you like to give?")
+  tip_value = int(tip_val)
+  print("\nAdding %f percent tip to a restaurant bill." %(tip_value))   
+  bill_value += ((bill_value * tip_value) / 100)
+  print ("\nWith %f percent tip: %f" %(tip_value,bill_value))
+  return (bill_value)
+
 my_bill_with_tax = tax(bill_value)
+my_bill_with_tip = tip(my_bill_with_tax)
