b66ff0d9dc5a3f65096a15a7c350759b80ae340d
S00167887 <S00167887@mail.itsligo.ie>
2019-10-08 23:25:08 +0100

    formatting numbers to display nicer

--- bill_calculator.py
+++ bill_calculator.py
@@ -4,16 +4,16 @@ bill_value = int(bill)
 def tax(bill_value):
   print("\nAdding 8% tax to a restaurant bill.")
   bill_value *= 1.08
-  print ("\nWith tax: %f" %(bill_value))
+  print (f"\nWith tax: {bill_value:.2f}")
   return (bill_value)
 
   
 def tip(bill_value):
   tip_val = input("\nWhat % of a tip would you like to give?")
   tip_value = int(tip_val)
-  print("\nAdding %f percent tip to a restaurant bill." %(tip_value))   
+  print(f"\nAdding {tip_value:.0f}% tip to a restaurant bill.")   
   bill_value += ((bill_value * tip_value) / 100)
-  print ("\nWith %f percent tip: %f" %(tip_value,bill_value))
+  print (f"\nWith {tip_value:.0f}% tip: {bill_value:.2f}")
   return (bill_value)
 
 my_bill_with_tax = tax(bill_value)
